ba9d75fb3be1ff0bfec76675c6f77336491c833f
Berend Sliedrecht <61358536+berendsliedrecht@users.noreply.github.com>
2024-10-27 09:04:15 +0100

    feat(android): use eudi mdoc data transfer library (#1)
    Signed-off-by: Berend Sliedrecht <sliedrecht@berend.io>

--- src/MdocDataTransferModule.ts
+++ src/MdocDataTransferModule.ts
@@ -1,5 +1,9 @@
 import { requireNativeModule } from 'expo-modules-core'
 
-// It loads the native module object from the JSI or falls back to
-// the bridge module (from NativeModulesProxy) if the remote debugger is on.
-export default requireNativeModule('MdocDataTransfer')
+type MdocNativeModule = {
+  hello: () => string
+}
+
+const mDocNativeModule = requireNativeModule<MdocNativeModule>('MdocDataTransfer')
+
+export const hello = mDocNativeModule.hello
