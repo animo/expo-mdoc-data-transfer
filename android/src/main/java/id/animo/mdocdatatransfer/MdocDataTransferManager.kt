f6570e6be5e4627a8097c7633f13d0609284820b
Berend Sliedrecht <61358536+berendsliedrecht@users.noreply.github.com>
2025-03-05 13:24:16 +0100

    feat: android update library (#15)
    Signed-off-by: Berend Sliedrecht <berend@animo.id>

--- android/src/main/java/id/animo/mdocdatatransfer/MdocDataTransferManager.kt
+++ android/src/main/java/id/animo/mdocdatatransfer/MdocDataTransferManager.kt
@@ -2,10 +2,11 @@ package id.animo.mdocdatatransfer
 
 import android.annotation.SuppressLint
 import android.content.Context
-import eu.europa.ec.eudi.iso18013.transfer.DocumentsResolver
+import com.android.identity.securearea.software.SoftwareSecureArea
+import com.android.identity.storage.EphemeralStorageEngine
 import eu.europa.ec.eudi.iso18013.transfer.TransferManager
-import eu.europa.ec.eudi.iso18013.transfer.response.ResponseGenerator
-import eu.europa.ec.eudi.iso18013.transfer.retrieval.BleRetrievalMethod
+import eu.europa.ec.eudi.iso18013.transfer.engagement.BleRetrievalMethod
+import eu.europa.ec.eudi.wallet.document.DocumentManager
 
 @SuppressLint("StaticFieldLeak")
 object MdocDataTransferManager {
@@ -16,20 +17,28 @@ object MdocDataTransferManager {
         this.context = context
     }
 
+    private val storageEngine = EphemeralStorageEngine()
+    private val secureArea = SoftwareSecureArea(storageEngine)
+
+    private val documentManager = DocumentManager
+        .Builder()
+        .setIdentifier("eudi_wallet_document_manager")
+        .setStorageEngine(storageEngine)
+        .addSecureArea(secureArea)
+        .build()
+
     val transferManager = lazy {
-        TransferManager.Builder(context).apply {
-            retrievalMethods = listOf(
+        TransferManager.getDefault(
+            context,
+            documentManager,
+            null,
+            listOf(
                 BleRetrievalMethod(
                     peripheralServerMode = true,
                     centralClientMode = true,
                     clearBleCache = true
                 )
             )
-            responseGenerator = ResponseGenerator.Builder(context)
-                .apply {
-                    documentsResolver = DocumentsResolver { listOf() }
-                }.build()
-
-        }.build()
+        )
     }
 }
